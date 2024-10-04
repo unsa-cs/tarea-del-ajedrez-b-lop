b30858c32cf40870a57d235625fa5f72e1e6f834
Gustavo <ryavar@unsa.edu.pe>
2024-10-03 22:24:26 -0500

    Se utilizo la funcion repeatH para dibujar la primerea fila

--- prob01.c
+++ prob01.c
@@ -2,22 +2,9 @@
 #include "figures.h"
 
 void display(){
-  // char** cblanco= repeatH(whiteSquare, 1);
-  // = repeatH(whiteSquare, 8);
-  // char** cnegro;
-
   char **base;
-
+  char **fila;
   base = join(whiteSquare,reverse(whiteSquare));
-  /*
-  for (int i = 0 ; i < 8 ; i++){
-    if ( i % 2 == 0 ){
-      cblanco =
-    }
-    else{
-      cnegro  = repeatH(reverse(whiteSquare), 1);
-    }    
-  }
-  */
-  interpreter(base);
+  fila = repeatH(base, 4);
+  interpreter(fila);
 }
