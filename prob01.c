e575f4d489820ef5706a007e9f73d135059a0394
Alejandro <alegonzales20022005@gmail.com>
2024-10-03 21:54:45 -0500

    Se intentó intercalar cuadrados haciendo uso de for, que cambiasen de color blanco a negro segun avanzaban las casillas.

--- prob01.c
+++ prob01.c
@@ -2,6 +2,14 @@
 #include "figures.h"
 
 void display(){
-  char** cblanco = repeatH(whiteSquare, 8);
+  char** cblanco;// = repeatH(whiteSquare, 8);
+  for (int i = 0 ; i < 8 ; i++){
+    if ( i % 2 == 0 ){
+      cblanco = repeatH(whiteSquare, 1);
+    }
+    else{
+      cblanco = repeatH(reverse(whiteSquare), 1);
+    }    
+  }
   interpreter(cblanco);
 }
