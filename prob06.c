7ab6ebc9a7af150e019551dc68ca7f94842d62e9
lizzz <ltitov@unsa.edu.pe>
2024-10-04 22:05:13 -0500

    se creo las primeras 6 filas correctamente, cambiando el color de la primera fila, agregando las casillas a los peones con superImpose y agregando 4 filas

--- prob06.c
+++ prob06.c
@@ -13,19 +13,26 @@ void display(){
   char **fila_peones;
   
   piezas_Blancas = join(rook, knight);
-  piezas_Blancas = join(piezas_Blancas, bishop);
 
+  piezas_Blancas = join(piezas_Blancas, bishop);
   piezas_Blancas = join(piezas_Blancas, queen);
   piezas_Blancas = join(piezas_Blancas, king);
   piezas_Blancas = join(piezas_Blancas, bishop);
   piezas_Blancas = join(piezas_Blancas, knight);
   piezas_Blancas = join(piezas_Blancas, rook);
 
-  fila_con_piezas = superImpose(reverse(piezas_Blancas),reverse(fila));
+  fila_con_piezas = superImpose(piezas_Blancas,reverse(fila));
   
   fila_peones = repeatH(pawn, 8);
+  fila_peones = superImpose(fila_peones,fila);
 
   fila_con_piezas = up(fila_con_piezas, fila_peones);
 
+  fila_con_piezas = up(fila_con_piezas, reverse(fila));
+  fila_con_piezas = up(fila_con_piezas, fila);
+
+  fila_con_piezas = up(fila_con_piezas, reverse(fila));
+  fila_con_piezas = up(fila_con_piezas, fila);
+  
   interpreter(fila_con_piezas);
 }
