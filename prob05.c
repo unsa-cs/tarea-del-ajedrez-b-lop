949708ca273c19a972c125dc7d790752c6b2e3bd
GustavoYG <ryavar@unsa.edu.pe>
2024-10-04 21:47:36 -0500

    Se utilizo la funcion reverse en las piezas para cambiar el color

---
+++ prob05.c
@@ -0,0 +1,26 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char **base_C;
+  char **fila;
+  base_C = join(whiteSquare,reverse(whiteSquare));
+  fila = repeatH(base_C, 4);
+
+  char **piezas_Blancas;
+  
+  char **fila_con_piezas;
+  
+  piezas_Blancas = join(rook, knight);
+  piezas_Blancas = join(piezas_Blancas, bishop);
+
+  piezas_Blancas = join(piezas_Blancas, queen);
+  piezas_Blancas = join(piezas_Blancas, king);
+  piezas_Blancas = join(piezas_Blancas, bishop);
+  piezas_Blancas = join(piezas_Blancas, knight);
+  piezas_Blancas = join(piezas_Blancas, rook);
+
+  fila_con_piezas = superImpose(reverse(piezas_Blancas),reverse(fila));
+
+  interpreter(fila_con_piezas);
+}
