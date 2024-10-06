4311331bf57f676102af27b7c6d497f41331a9af
lizzz <ltitov@unsa.edu.pe>
2024-10-05 22:36:19 -0500

    Realizamos el primer cuadrante para rotarlo y obtener el tablero simetrico

---
+++ prob07.c
@@ -0,0 +1,25 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char **cuadrante[4];
+  char **fila[4];
+  
+  fila[0] = repeatH(reverse(whiteSquare), 4);
+  fila[1] = join(reverse(whiteSquare),repeatH(whiteSquare, 3));
+  fila[2] = join(reverse(whiteSquare), whiteSquare);
+  fila[2] = join(fila[2], repeatH(reverse(whiteSquare), 2));
+  fila[3] = join(reverse(whiteSquare), whiteSquare);
+  fila[3] = repeatH(fila[3], 2);
+  
+  for(int i = 0; i < 3; i++){
+    if( i == 0){
+      cuadrante[0] = up(fila[i], fila[i+1]);
+    }
+    else{
+      cuadrante[0] = up(cuadrante[0], fila[i+1]);
+    }
+  }
+    
+  interpreter(cuadrante[0]);
+}
