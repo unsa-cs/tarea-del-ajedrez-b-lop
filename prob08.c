f246bb3440e79a498f788b2be0eb74727ab0f655
GustavoYG <ryavar@unsa.edu.pe>
2024-10-06 10:53:02 -0500

    Para la construccion del tablero se implemento un for anidado, salio un error de segmentacion

--- prob08.c
+++ prob08.c
@@ -4,13 +4,51 @@
 void display(){
   char **fila[8];
   char **tablero;
+
+  char **blackSquare = reverse(whiteSquare);
   
-  char **c2 = join(whiteSquare, reverse(whiteSquare));
-  char **c3 = join(c2, whiteSquare);
-  char **c4 = join(c3, reverse(whiteSquare));
+  int pos[8] = {4,1,3,6,2,7,5,0};
   
-  fila[0] = join(c4, superImpose(queen, whiteSquare));
-  fila[0] = join(fila[0], reverse(c3));
+  for(int i=0;i<8;i++){
+    if( i%2 == 0 ){
+      for(int j=0;j<8;j++){
+        if(pos[i]==j && j==0){
+          fila[i]=superImpose(queen,whiteSquare);
+        }
+        else if(pos[i]==j && j%2!=0) {
+          fila[i]=join(fila[i],superImpose(queen,blackSquare));
+        }
+        else if(pos[i]==j){
+          fila[i]=join(fila[i],superImpose(queen,whiteSquare));
+        }
+        else if(j%2==0){
+          fila[i]=join(fila[i],whiteSquare);
+        }
+        else{
+          fila[i]=join(fila[i],blackSquare);
+        }
+      }
+    }
+    else{
+      for(int j=0;j<8;j++){
+        if(pos[i]==j && j==0){
+          fila[i]=superImpose(queen,blackSquare);
+        }
+        else if(pos[i]==j && j%2!=0) {
+          fila[i]=join(fila[i],superImpose(queen,whiteSquare));
+        }
+        else if(pos[i]==j){
+          fila[i]=join(fila[i],superImpose(queen,blackSquare));
+        }
+        else if(j%2==0){
+          fila[i]=join(fila[i],blackSquare);
+        }
+        else{
+          fila[i]=join(fila[i],whiteSquare);
+        }
+      }
+    }
+  }
   
   interpreter(fila[0]);
 }
